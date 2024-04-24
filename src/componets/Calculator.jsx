aa18f487eb7ec86b3640bee6e06ccdccea97ef5a
atomic <baileyweb04@gmail.com>
2024-04-24 14:05:08 -0400

    calculator works

--- src/componets/Calculator.jsx
+++ src/componets/Calculator.jsx
@@ -4,10 +4,28 @@ function Calculator(){
   const [count, setCount] = useState([]);
   
 
-  function calculate(nums){
+  function calculate(nums, thisOp){
     switch(nums){
       case nums:
         setCount([...count, nums]) 
+      break;
+    }
+
+    switch(thisOp){
+      case '+':
+        count.includes("+") ? finalOutput("add") : null
+      break;
+      case '-':
+        count.includes("-") ? finalOutput("sub") : null
+      break;
+      case '*':
+        count.includes("*") ? finalOutput("multi") : null
+      break;
+      case '/':
+        count.includes("/") ? finalOutput("divide") : null
+      break;
+      case '%':
+        count.includes("%") ? finalOutput("percent") : null
       break;
     }
   }
@@ -18,13 +36,63 @@ function Calculator(){
         setCount(prevState => prevState.slice(0, -1))
       break;
       case 'clear':
-        setCount([...count.slice(0, 0)]) 
+        setCount([]) 
       break;
     }
   }
 
-  const finalOutput = () => {
+  //Calculates the result based on the numbers in the array and which operator you used
+  const finalOutput = (op) => {
+    let index;
+    let opindex;
+    let result;
 
+    switch (op) {
+      case "add":
+        opindex = count.indexOf("+");
+      break;
+      case "sub":
+        opindex = count.indexOf("-");
+      break;
+      case "multi":
+        opindex = count.indexOf("*");
+      break;
+      case "divide":
+        opindex = count.indexOf("/");
+      break;
+      case "percent":
+        opindex = count.indexOf("%");
+      break;
+    }
+
+    const leftArray = count.slice(0, opindex);
+    const rightArray = count.slice(opindex + 1);
+
+    const left = parseFloat(leftArray.join(""));
+    const right = parseFloat(rightArray.join(""));  
+
+    index = count.indexOf(`${op}`);
+    count.splice(index, 1, null);
+
+    switch(op){
+      case "add":
+        result = left + right
+      break;
+      case "sub":
+        result = left - right
+      break;
+      case "multi":
+        result = left * right
+      break;
+      case "divide":
+        result = left / right
+      break;
+      case "percent":
+        result = `${left / right * 100}%`
+      break;
+    }
+
+    setCount([result])
   }
 
   return(
@@ -34,8 +102,8 @@ function Calculator(){
       </div>
       <div className="calc-row-1">
           <li><button onClick={() => modifyNum('clear')}>C</button></li>
-          <li><button onClick={() => calculate('/')}>/</button></li>
-          <li><button onClick={() => calculate('%')}>%</button></li>
+          <li><button onClick={() => calculate('/', "/")}>/</button></li>
+          <li><button onClick={() => calculate('%', "%")}>%</button></li>
           <li><button onClick={() => modifyNum('backspace')}>Back</button></li>
         </div>
       <div className="calc-btn">
@@ -58,14 +126,14 @@ function Calculator(){
         <ul>
           <li><button>+/-</button></li>
           <li><button onClick={() => calculate(0)}>0</button></li>
-          <li><button onClick={() => calculate('.')}>.</button></li>
+          <li><button onClick={() => calculate('.', ".")}>.</button></li>
         </ul>
         </div>
         <div className="calc-col-2">
         <ul>
-          <li><button onClick={() => calculate('*')}>x</button></li>
-          <li><button onClick={() => calculate('-')}>-</button></li>
-          <li><button onClick={() => calculate('+')}>+</button></li>
+          <li><button onClick={() => calculate('*', "*")}>*</button></li>
+          <li><button onClick={() => calculate('-', "-")}>-</button></li>
+          <li><button onClick={() => calculate('+', "+")}>+</button></li>
           <li><button onClick={() => finalOutput()}>=</button></li>
         </ul>
       </div>
